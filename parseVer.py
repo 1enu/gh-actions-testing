b306b1f693dc0af1089127c2d2e85f692393c991
EnU <140404651+1enu@users.noreply.github.com>
2024-10-02 12:54:53 +0800

    edit typo

--- parseVer.py
+++ parseVer.py
@@ -4,7 +4,7 @@ import re
 
 data = sys.argv[1]
 bump = re.search(r"\"bump\":\"([^\"]+)\"", data).group(1)
-version = bump = re.search(r"\"currentVersion\":\"([^\"]+)\"", data).group(1)
+version = re.search(r"\"currentVersion\":\"([^\"]+)\"", data).group(1)
 result = subprocess.run(
     ['bumpver', 'test', f"--{bump}", version, "MAJOR.MINOR.PATCH"],
     capture_output = True, # Python >= 3.7 only
